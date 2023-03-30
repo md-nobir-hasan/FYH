d2d707fced81d41d2b91caedab036286dee73a70
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-03-30 12:40:10 +0600

    sweet alert implement done

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,4 @@
 <footer>
     {{-- there is no footer, Create footer here  --}}
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- resources/views/user/user/index.blade.php
+++ resources/views/user/user/index.blade.php
@@ -68,7 +68,7 @@
                                               </svg>
                                         </a>
                                         <a href="{{route('admin.delete',[$value->id,'User'])}}"
-                                            class="ml-2 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            class="ml-2 delete font-medium text-blue-600 dark:text-blue-500 hover:underline">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-[red]">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                               </svg>
@@ -83,7 +83,28 @@
             </div>
             <x-slot:js>
                 <script type="module">
-
-    </script>
+                    $('.delete').on('click',function(e){
+                       e.preventDefault();
+                        let url = $(this).prop('href');
+                        Swal.fire({
+                            title: 'Are you sure?',
+                            text: "You won't be able to revert this!",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Yes, delete it!'
+                            }).then((result) => {
+                                window.location = url;
+                            // if (result.isConfirmed) {
+                            //     Swal.fire(
+                            //     'Deleted!',
+                            //     'Your file has been deleted.',
+                            //     'success'
+                            //     )
+                            // }
+                        })
+                    })
+                </script>
             </x-slot:js>
 </x-app-layout>
