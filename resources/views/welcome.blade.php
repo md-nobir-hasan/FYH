ca83dff0b9f7301ec3f4d4ae61c39645c702045b
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-04-07 16:48:33 +0600

    dfsd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="">
                     <div class="">
-                        <a href="#" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <a href="{{route('login')}}" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Welcome to FYH</h1>
                             </div>
