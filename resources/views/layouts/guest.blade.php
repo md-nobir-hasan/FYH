a1f1fd6ec859a886fe60a7e4b20685ce4f60e0e2
nhn1998 <nhnayeem1@gmail.com>
2023-05-22 16:07:05 +0600

    register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
