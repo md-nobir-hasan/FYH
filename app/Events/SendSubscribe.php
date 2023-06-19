2cb05fd6c0968c63d04656d66c5de60e3c3354f0
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-20 05:25:20 +0600

    Thankyou error fixing (#64) (#65)
    * three page create and  navigation add
    
    * subscribe  add
    
    * mail subscribe and redirect problem done
    
    ---------
    
    Co-authored-by: Mujan9856 <mdmujibur741@gmail.com>

---
+++ app/Events/SendSubscribe.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SendSubscribe
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public $email;
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
