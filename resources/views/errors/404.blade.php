581ee475bda0850d36f13ab2dc5b3f329a059fb6
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-21 12:49:25 +0600

    route added (#67)

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 not found</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <h1 class="text-center">404 not fond</h1>
+</body>
+</html>
