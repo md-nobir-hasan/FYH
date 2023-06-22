1906a492b4a131ead3a82736e37b97221afce892
nhn1998 <nhnayeem1@gmail.com>
2023-06-22 15:24:49 +0600

    error and guide design

--- resources/views/frontend/pages/survival.blade.php
+++ resources/views/frontend/pages/survival.blade.php
@@ -4,8 +4,42 @@
 @endpush
 @section('content')
 
-<div>
-    <h1 class="text-center mt-10">This is survival guide page</h1>
+<div style="font-family:'Poppins'" class="mb-[-100px]">
+    <section class="mt-5">
+        <img src="/images/sguide.png" alt="" class=""/>
+    </section>
+    <section class="mt-5 lg:mx-40">
+    <video class="w-full h-[400px]" width="" controls>
+  <source src="" type="video/mp4">
+</video>
+<h1 class="text-5xl font-black mt-2">Survival Guide Video</h1>
+<p class="text-lg uppercase">Guidence from Thomas tsangaras</p>
+    </section>
+    <section class="mt-20">
+        <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 lg:mx-36 mx-5 gap-5">
+            <div>
+                <video class="rounded-xl w-full" width="" controls>
+                <source src="" type="video/mp4">
+            </video>
+            <h1 class="font-bold text-lg uppercase mt-4 ml-2">Guidence from Thomas tsangaras</h1>
+            <p class="ml-2">Here goes a short description about this video. </p>
+            </div>
+            <div>
+                <video class="rounded-xl w-full" width="" controls>
+                <source src="" type="video/mp4">
+            </video>
+            <h1 class="font-bold text-lg uppercase mt-4 ml-2">Guidence from Thomas tsangaras</h1>
+            <p class="ml-2">Here goes a short description about this video. </p>
+            </div>
+            <div>
+                <video class="rounded-xl w-full" width="" controls>
+                <source src="" type="video/mp4">
+            </video>
+            <h1 class="font-bold text-lg uppercase mt-4 ml-2">Guidence from Thomas tsangaras</h1>
+            <p class="ml-2">Here goes a short description about this video. </p>
+            </div>
+        </div>
+    </section>
 </div>
 
 @endsection
