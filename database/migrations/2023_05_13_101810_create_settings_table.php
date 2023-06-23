11eaefee3c8d0d5ff84198425a64752675182617
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-23 16:24:41 +0600

    build delete

--- database/migrations/2023_05_13_101810_create_settings_table.php
+++ database/migrations/2023_05_13_101810_create_settings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('trail')->nullable();
+            $table->integer('user_trail')->nullable();
             $table->string('logo');
             $table->string('email')->nullable();
             $table->longText('address')->nullable();
