350ee48fc12a357e50b319a06964e905df6e13ec
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-27 12:17:33 +0600

    some error fixing

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -16,7 +16,9 @@
             <h1 class="text-4xl font-black text-[#D1052C] mt-5 text-center">Oops... Page Not Found!</h1>
             <p class="text-center">Please return to the site homepage . It looks like nothing was found at this locaion. </p>
             <div class="flex justify-center items-center mt-3 mb-20">
-            <a class="bg-[#D1052C] text-white px-5 py-2 rounded-lg cursor-pointer">Back To Home</a>
+            <a href="@auth
+                {{route('user.home')}} @else {{route('home')}}
+            @endauth" class="bg-[#D1052C] text-white px-5 py-2 rounded-lg cursor-pointer">Back To Home</a>
             </div>
         </div>
     </section>
