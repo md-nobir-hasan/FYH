1c2d0072f322681f7e88d66006854f5b13b8b233
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-08-12 13:36:38 +0600

    payment subscription notification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Console;
 
+use App\Models\Payment;
+use App\Models\UserNotification;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Auth;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +18,33 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+            try {
+                $today = Carbon::now();
+                $date = $today->addDays(9);
+                $payments = Payment::where('end_time', '>=',$today)->where('end_time', '<', $date)->get();
+                if (count($payments) > 0) {
+                    foreach ($payments as $payment) {
+                        $end_time = Carbon::parse($payment->end_time);
+                        $days = $end_time->diffIndays($today);
+                        $msg = "$days days remain to expire your subscription";
+                        if($days == 0){
+                            $msg = "Your subscription is expired";
+                        }
+                        UserNotification::create([
+                            'user_id' => $payment->user_id,
+                            'msg' => $msg,
+                        ]);
+                    }
+                }
+            } catch (Exception $e) {
+                UserNotification::create([
+                    'user_id' => 1,
+                    'msg' => "$e",
+                ]);
+            }
+        });
     }
 
     /**
@@ -20,7 +52,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
