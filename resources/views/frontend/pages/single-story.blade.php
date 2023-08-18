f666ef0aa87239889b4607d8cf9fe6a76d65662b
nhn1998 <nhnayeem1@gmail.com>
2023-08-18 11:25:31 +0600

    home responsive

--- resources/views/frontend/pages/single-story.blade.php
+++ resources/views/frontend/pages/single-story.blade.php
@@ -17,11 +17,12 @@
 
                     <div>
                         @auth
-                            <svg class="mt-[10px] ml-[10px] cursor-pointer" width="20" height="20"
+                        
+                            <svg class="mt-[10px] ml-[10px] cursor-pointer border-[#666565]" width="20" height="20"
                                 data-id="{{ $story->id }}" id="wl_manage" onclick="WishlistManage()"
                                 xmlns="http://www.w3.org/2000/svg"
-                                @if ($wishlist = $story->wishlists->where('user_id', Auth::user()->id)->first()) fill="red" data-add='' @else fill="none" data-add='yes' @endif
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                @if ($wishlist = $story->wishlists->where('user_id', Auth::user()->id)->first()) fill=[#D1052C] data-add='' @else fill="none" data-add='yes' @endif
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 border-[#D1052C]">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                             </svg>
