7a6d372aaf4ec8231124b9d276ff1cb80444ddb0
nhn1998 <nhnayeem1@gmail.com>
2023-08-20 02:06:27 +0600

    responsive pages

--- resources/views/frontend/pages/help_support.blade.php
+++ resources/views/frontend/pages/help_support.blade.php
@@ -3,32 +3,60 @@
     Help and support
 @endpush
 @section('content')
+<style>
+    .hbg{
+        background:url('/images/reviewbg.png');background-repeat:no-repeat;background-size:100% 402px
+    }
+    @media (min-width: 0px) and (max-width: 1023px){
+        .hbg{
+            background:none
+        }
+    }
+    div:nth-child(1)>img{
+       padding:19px;
+    }
+    div:nth-child(2)>img{
+       padding:19px 21px 19px 21px;
+    }
+    div:nth-child(3)>img{
+       padding:19px;
+    }
+    div:nth-child(4)>img{
+       padding:17px 20px;
+    }
+    div:nth-child(5)>img{
+       padding:19px;
+    }
+    div:nth-child(6)>img{
+       padding:19px;
+    }
+</style>
 <div class="" style="font-family:'Poppins'">
-<section class="mt-[65px]">
-<div>
+<section class="md:mt-[65px]">
+<div class="lg:mx-10 2xl:mx-0">
 
-   <a href="{{route('ticket')}}">
-    <img src="{{'/storage/'. $tittles->help_image}}" alt="" class="w-[1320px] mx-auto"/>
+   <a href="{{route('ticket')}}" >
+    <img src="{{'/storage/'. $tittles->help_image}}" alt="" class="2xl:w-[1320px] w-full lg:h-[299px] md:h-[220px] h-[150px] mx-auto"/>
 </a>
 
 </div>
-<div class="" style="background:url('/images/reviewbg.png');background-repeat:no-repeat;background-size:100% 397px">
+<div class="hbg" style="">
 <div class="mt-[50px]  lg:pt-[28px]">
-    <h1 class="text-[50px] font-bold text-center leading-normal"> @if($tittles !==null)  {{$tittles->help_title}} {{auth()->user()->fname}} @endif</h1>
-    <p class="text-center text-[24px] leading-normal font-normal "> @if($tittles !==null)  {{$tittles->help_subtile}}  @endif </p>
+    <h1 class="lg:text-[50px] text-[36px] leading-[40px] font-bold text-center lg:leading-normal"> @if($tittles !==null)  {{$tittles->help_title}} {{auth()->user()->fname}} @endif</h1>
+    <p class="text-center lg:text-[24px] text-[18px] leading-normal font-normal "> @if($tittles !==null)  {{$tittles->help_subtile}}  @endif </p>
     <div class=" mb-[-100px]">
 
             <!-- benifit card section -->
-            <div class="w-[1320px] mx-auto grid grid-cols-3 gap-6 mt-[30px]">
+            <div class="2xl:w-[1320px] 2xl:mx-auto lg:mx-10 mx-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-[30px]">
 
          @if($helps->count() > 0)
          @foreach ($helps as $help)
              
       
-       <div class="h-[232px] w-[424px] px-[80px] py-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+       <div class="h-[232px] overflow-hidden w-full 2xl:w-[424px] px-[80px] py-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
             <img src="{{'/storage/'. $help->image}}" alt="" class="h-[80px] w-[80px] mx-auto mt-[16px] rounded-full" style="box-shadow: 0px 2px 5px 2px rgba(0, 0, 0, 0.08);">
-            <h5 class="mb-2 text-xl font-semibold mt-[5px] tracking-tight text-gray-900  text-center"> {{$help->title}} </h5>
-            <p class="font-medium text-sm text-justify mt-[5px]">
+            <h5 class=" text-xl font-semibold mt-[5px] tracking-tight text-gray-900  text-center"> {{$help->title}} </h5>
+            <p class="font-medium text-sm xl:mx-[-25px] lg:mx-[-35px] mx-[-40px] text-justify  mt-[5px]">
                    {{$help->description}}
             </p>
         </div>
--- resources/views/frontend/pages/problem.blade.php
+++ resources/views/frontend/pages/problem.blade.php
@@ -4,11 +4,11 @@
 @endpush
 @section('content')
 
-<section class="mt-[67px] w-[1320px] mx-auto bg-white mb-[-150px]" style="font-family:'Poppins'">
+<section class="md:mt-[67px] mt-[30px] 2xl:w-[1320px] mx-auto bg-white mb-[-150px]" style="font-family:'Poppins'">
 
-    <div class="border px-20 py-14">
+    <div class="border 2xl:mx-0 md:mx-10 mx-5 md:px-20 px-5 py-14">
         <div class="">
-            <a href="/ticket" class=""><button class=" flex items-center px-2 py-2 shadow-lg"><span class="mr-1"><svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <a href="/ticket" class=""><button class=" flex items-center px-5 py-3 text-[#848484] text-[16px] font-bold hover:bg-[#F6CDD5] hover:text-[#D1052C] shadow-lg"><span class="mr-1"><svg class="text-[#848484] hover:text-[#D1052C]" width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M0.292892 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292892 7.29289ZM17 7L1 7V9L17 9V7Z" fill="#666565"/>
     </svg>
     </span>Back</button></a>
